3ebe41ee07b2442a60d0c2b71c1c6a8144085b8c
HayoBK <hbreinbauer@uchile.cl>
2022-10-26 15:54:39 -0300

    Mejor y Mejor

--- main.py
+++ main.py
@@ -12,10 +12,14 @@ sns.swarmplot(data=df_X,x='Momento',y='THI')
 plt.show()
 
 #%%
-B=[17,37,57,77]
-sns.displot(data=df_X, x='THI',hue='Momento', bins=B, stat='density',common_norm=False) #, common_norm=False,element='step', fill=False)
+B=[0,17,37,57,77,100]
+sns.displot(data=df_X, x='THI',hue='Momento', bins=B, stat='percent',multiple='layer', common_norm=False) #, common_norm=False,element='step', fill=False)
 #sns.displot(data=df_X, x='THI',hue='Momento',bins=B ,kind='kde', common_norm=False)
 
 plt.show()
 
+#%% Bin data --> no lo usaré
+df_X['THI (nivel)'] = pd.cut( x= df_X['THI'], bins=[0,17,37,57,77,100], labels = ['Muy leve','Leve','Moderado','Severo','Catrastrófico'])
+sns.countplot(data=df_X, x='THI (nivel)', hue='Momento')
+plt.show()
 print(df_T)
